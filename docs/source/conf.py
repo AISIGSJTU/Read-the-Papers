2d9aecf815a61889eca7a72bb15715dd3b6b4171
qbdl <1260017352@qq.com>
2023-11-14 21:23:21 +0800

    initialize the first paper sharing

---
+++ docs/source/conf.py
@@ -0,0 +1,57 @@
+# Configuration file for the Sphinx documentation builder.
+import sphinx_rtd_theme
+import maisie_sphinx_theme
+# extensions.append("maisie_sphinx_theme")
+
+# -- Project information
+
+project = 'Reading Share'
+copyright = '2021, Graziella'
+author = 'Li Kejie'
+
+release = '1.0'
+version = '1.0.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'recommonmark',
+    'sphinx_markdown_tables'
+]
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_material'
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = 'maisie_sphinx_theme'
+# html_theme_path = maisie_sphinx_theme.html_theme_path()
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
